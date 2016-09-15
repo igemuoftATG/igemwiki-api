68bb975a05311b2a985c5fe3b3d101c4997973eb
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-09-15 09:49:26 -0400

    test stylesheet

--- test/upload.spec.js
+++ test/upload.spec.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const path = require('path')
+const Promise = require('bluebird')
+const fs = Promise.promisifyAll(require('fs'))
 
 const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
@@ -10,18 +12,41 @@ should()
 
 const { login, upload } = require('../index.js')({ teamName: 'Toronto' })
 
+const fillVars = (str) => str
+  .replace('{{ YEAR }}', new Date().getFullYear())
+  .replace('{{ TEAM_NAME }}', 'Toronto')
+  .replace('{{ DATE }}', new Date().toString())
+
 describe('Upload', function() {
-  it('Should upload a page', function() {
+  it('Should upload a stylesheet', function() {
     this.timeout(10 * 1000)
 
     return login().then(jar => upload({
-      type: 'page',
-      pageOrImageName: 'test-upload',
-      fileName: path.resolve(__dirname, './files/test-upload.html'),
+      type: 'stylesheet',
+      pageOrImageName: 'test-stylesheet',
+      fileName: path.resolve(__dirname, './files/test-stylesheet.css'),
       dir: path.resolve(__dirname, 'responses'),
       jar
     })).then((responseDetails) => {
       console.log('responseDetails: ', responseDetails)
     })
+  })
+
+  it('Should upload a page', function() {
+    this.timeout(10 * 1000)
+
+    return fs.readFileAsync(path.resolve(__dirname, './files/test-upload-template.html'), 'utf-8')
+      .then(content => fillVars(content))
+      .then(content => fs.writeFileAsync(path.resolve(__dirname, './files/test-upload.html'), content))
+      .then(() => login())
+      .then(jar => upload({
+        type: 'page',
+        pageOrImageName: 'test-upload',
+        fileName: path.resolve(__dirname, './files/test-upload.html'),
+        dir: path.resolve(__dirname, 'responses'),
+        jar
+      })).then((responseDetails) => {
+        console.log('responseDetails: ', responseDetails)
+      })
   })
 })

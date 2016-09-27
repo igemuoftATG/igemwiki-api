33830ebaae9ac029bfb70de26eb5bc41024b2e7c
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-09-27 14:34:48 -0400

    fix download pages test and rearrange download spec files

--- test/download.spec.js
+++ test/download.spec.js
@@ -10,25 +10,24 @@ chai.use(chaiAsPromised)
 const { assert, should } = chai
 should()
 
-const { downloadPage, getTeamPages } = require('../index.js')({ teamName: 'Toronto' })
+const { getTeamPages, downloadPage, downloadAll } = require('../index.js')({ teamName: 'Toronto' })
 
-describe('Download page', function() {
+describe('Download', function() {
   it('Should download all pages from getTeamPages', function() {
     this.timeout(15 * 1000)
 
     const dir = path.resolve(__dirname, 'download')
 
-    let expectedLength
 
     return getTeamPages()
-      .then((pages) => {
-        expectedLength = pages.length
+      .then(pages => Promise.all(pages.map(page => downloadPage({ page, dir }))))
+  })
 
-        return Promise.all(pages.map(page => downloadPage({ page, dir })))
-      })
-      .then(() => fs.readdirAsync(dir))
-      .then((files) => {
-        assert.isOk(files.length === expectedLength, 'Did not download all pages')
-      })
+  it('Should download all pages and templates', function() {
+    this.timeout(30 * 1000)
+
+    return downloadAll({ dir: path.resolve(__dirname, './download') }).then((results) => {
+      console.log('results: ', results)
+    })
   })
 })
